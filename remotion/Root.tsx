ad602f03321de899868a91c35b86f63a8e92589e
Tarun <itstarun1994@gmail.com>
2026-04-09 01:15:12 +0530

    feat: add 3 feature highlight Remotion compositions (git-native, mdx-editor, workflow)

--- remotion/Root.tsx
+++ remotion/Root.tsx
@@ -1,8 +1,37 @@
 import { Composition } from "remotion"
+import { GitNativeFeature } from "./compositions/GitNativeFeature"
+import { MDXEditorFeature } from "./compositions/MDXEditorFeature"
 import { StudioDemo } from "./compositions/StudioDemo"
+import { WorkflowFeature } from "./compositions/WorkflowFeature"
 
 export const RemotionRoot = () => {
   return (
-    <Composition id="StudioDemo" component={StudioDemo} durationInFrames={300} fps={30} width={1280} height={720} />
+    <>
+      <Composition id="StudioDemo" component={StudioDemo} durationInFrames={300} fps={30} width={1280} height={720} />
+      <Composition
+        id="GitNativeFeature"
+        component={GitNativeFeature}
+        durationInFrames={150}
+        fps={30}
+        width={1280}
+        height={720}
+      />
+      <Composition
+        id="MDXEditorFeature"
+        component={MDXEditorFeature}
+        durationInFrames={210}
+        fps={30}
+        width={1280}
+        height={720}
+      />
+      <Composition
+        id="WorkflowFeature"
+        component={WorkflowFeature}
+        durationInFrames={150}
+        fps={30}
+        width={1280}
+        height={720}
+      />
+    </>
   )
 }
